827530d5e9210f7517de83f748793a3232500a5a
dongyeon-0822 <myjjue00@gmail.com>
2020-12-17 14:16:31 +0900

    Update divArray.cpp and noSameNum.cpp

---
+++ Level1/noSameNum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+
+    for (int i = 0; i < arr.size(); i++) {
+        if (i==0 || answer.back() != arr[i])
+            answer.push_back(arr[i]);
+    }
+
+    return answer;
+}
