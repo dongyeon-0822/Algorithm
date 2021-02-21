6a55159619c83f707063fd947a99bb5ae11bdd46
dongyeon-0822 <myjjue00@gmail.com>
2021-02-22 02:19:29 +0900

    2021/02/22 Update carsarPW.py

--- Programmers/Level1_practice/carsarPW.py
+++ Programmers/Level1_practice/carsarPW.py
@@ -1,8 +1,12 @@
 def solution(s, n):
     answer = ''
     for i in s:
-        if i=='A':
-            print(i)
+        if i==' ':
+            answer+=' '
+        elif 'a'<=i<='z':
+            answer+=chr((ord(i)+n-97)%26+97)
+        elif 'A' <= i <= 'Z':
+            answer+=chr((ord(i)+n-65)%26+65)
     return answer
 
 if __name__=='__main__':
