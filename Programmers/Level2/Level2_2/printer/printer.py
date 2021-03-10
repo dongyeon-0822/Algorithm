8e99c1812f456133fff270cfca6340f49c70d78a
dongyeon-0822 <myjjue00@gmail.com>
2021-03-11 01:43:34 +0900

    2021/03/11 Update printer.py

--- Programmers/Level2/Level2_2/printer/printer.py
+++ Programmers/Level2/Level2_2/printer/printer.py
@@ -5,7 +5,14 @@ def solution(priorities, location):
         queue.append(i)
     while True:
         _max=max(priorities)
-        if priorities[queue[0]]==_max:
-
-
-    return answer
+        if priorities[0]==_max:
+            answer+=1
+            if queue[0]==location:
+                return answer
+            del priorities[0]
+            del queue[0]
+        else:
+            priorities.append(priorities[0])
+            queue.append(queue[0])
+            del priorities[0]
+            del queue[0]
