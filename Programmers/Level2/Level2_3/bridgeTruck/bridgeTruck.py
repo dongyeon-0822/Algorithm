7c78e6b1bf9d2d48c5d4e0752d3a552e97acdc70
dongyeon-0822 <myjjue00@gmail.com>
2021-03-13 02:04:21 +0900

    2021/03/13 Update bridgeTruck.py

--- Programmers/Level2/Level2_3/bridgeTruck/bridgeTruck.py
+++ Programmers/Level2/Level2_3/bridgeTruck/bridgeTruck.py
@@ -4,11 +4,18 @@ def solution(bridge_length, weight, truck_weights):
 
     on_bridge=[]
     on_weight=0
-
-    while not truck_weights.empty():
+    while len(truck):
+        if on_weight+truck[0][1]<=weight:
+            on_weight+=truck[0][1]
+            on_bridge.append(truck[0])
+            del truck[0]
+        for i in on_bridge:
+            i[0]+=1
+            if i[0]==bridge_length:
+                on_weight-=i[1]
+                del on_bridge[0]
         answer+=1
-        for i,j in on_bridge:
+    return answer+bridge_length
 
-
-
-    return answer
+if __name__=='__main__':
+    print(solution(2,10,[7,4,5,6]))
