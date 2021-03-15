b2750d6f893a6f39ced4a310d7ca3291d2480bff
dongyeon-0822 <myjjue00@gmail.com>
2021-03-16 01:06:45 +0900

    2021/03/16 Update triSnail.py

--- Programmers/Level2/Level2_4/triSnail/triSnail.py
+++ Programmers/Level2/Level2_4/triSnail/triSnail.py
@@ -2,9 +2,26 @@ def solution(n):
     answer = []
     arr=[[0 for j in range(i+1)] for i in range(n)]
 
-    x,y,num=0,0,0
+    x,y,num=-1,0,1
     for i in range(n):
-        if i%3==1:
+        if i%3==0:
             for j in range(n-i):
+                x+=1
                 arr[x][y]=num
+                num+=1
+        elif i%3==1:
+            for j in range(n-i):
+                y+=1
+                arr[x][y]=num
+                num+=1
+        else:
+            for j in range(n-i):
+                x-=1
+                y-=1
+                arr[x][y]=num
+                num+=1
+    for i in arr:
+        answer.extend(i)
     return answer
+
+print(solution(5))
