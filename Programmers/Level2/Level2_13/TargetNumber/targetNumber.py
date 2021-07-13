9bde0284b248572a47158c0bcaad1c1568e52061
DongYeon Kang <myjjue00@gmail.com>
2021-07-14 03:44:32 +0900

    Update targetNumber.py

--- Programmers/Level2/Level2_13/TargetNumber/targetNumber.py
+++ Programmers/Level2/Level2_13/TargetNumber/targetNumber.py
@@ -1,23 +1,22 @@
-answer = 0
-def dfs(numbers, target, result, count, answer, sign):
+arr=[]
+def dfs(numbers, target, result, count, sign):
     if sign: result += numbers[count]
     else: result -= numbers[count]
     count += 1
     if count == len(numbers):
         if result==target:
-            answer+=1
+            arr.append(1)
         return
     else:
-        dfs(numbers, target, result, count, answer, 1)
-        dfs(numbers, target, result, count, answer, 0)
+        dfs(numbers, target, result, count, 1)
+        dfs(numbers, target, result, count, 0)
 
 def solution(numbers, target):
-    global answer
     result = 0
     count = 0
-    dfs(numbers, target, result, count, answer, 1)
-    dfs(numbers, target, result, count, answer, 0)
-    return answer
+    dfs(numbers, target, result, count, 1)
+    dfs(numbers, target, result, count, 0)
+    return len(arr)
 
 if __name__ == '__main__':
     solution([1,1,1,1,1],3)
