dcf3313af4047bd9712373e6a6a44ae08b4ad257
DongYeon Kang <myjjue00@gmail.com>
2021-08-15 23:59:03 +0900

    Create 2745_changeBase.py

---
+++ Baekjoon/Math/2745_changeBase.py
@@ -0,0 +1,13 @@
+import string
+
+tmp = string.digits+string.ascii_lowercase
+def convert(num, base) :
+    q, r = divmod(num, base)
+    if q == 0 :
+        return tmp[r]
+    else :
+        return convert(q, base) + tmp[r]
+
+if __name__ =='__main__':
+    N,B = map(int,input().split())
+    convert(int())
