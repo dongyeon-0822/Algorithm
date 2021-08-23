c50c7677ca385f60cccdb8af777addef196e3c41
DongYeon Kang <myjjue00@gmail.com>
2021-08-23 23:55:52 +0900

    Update candidateKey.py

--- Programmers/Level2/Level2_13/CandidateKey/candidateKey.py
+++ Programmers/Level2/Level2_13/CandidateKey/candidateKey.py
@@ -2,18 +2,30 @@ from itertools import combinations
 
 def solution(relation):
     answer = 0
-    key = [i for i in range(len(relation[0]))]
+    col = [i for i in range(len(relation[0]))]
     c = [0 for i in range(len(relation[0]))]
     for j in range(len(relation[0])):
         c[j] = [r[j] for r in relation]
         if len(c[j])==len(set(c[j])):
             answer+=1
-            key.remove(j)
-    # 인덱스가 아닌 것끼리 2부터 조합 구해서 튜플로 만들어서 중복 체크!
-    com = []
-    for i in range(2,len(key)+1):
-        com.extend(list(combinations(key,i)))
-    for c in com:
-        []
+            col.remove(j)
+    comb = [] # 인덱스의 조합(2 이상)
+    comb_key = []
+    for i in range(2,len(col)+1):
+        comb.extend(list(combinations(col,i)))
+    for i in comb:
+        arr = [[0]*len(i) for _ in range(len(relation))]
+        for j in range(len(i)):
+            for k in range(len(arr)):
+                arr[k].append(c[i[j]][k])
+        if len(arr)==len(set(arr)):
+            flag = True
+            for ck in comb_key:
+                if set(ck).issubset(set(i)):
+                    flag=False
+            if flag:
+                comb_key.append(i)
 
-    return answer
+    return answer+len(comb_key)
+
+solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]])
