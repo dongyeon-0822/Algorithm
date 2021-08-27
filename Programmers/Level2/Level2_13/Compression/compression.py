86a46a7087dbf869f75cf24e1b0c09564202d278
DongYeon Kang <myjjue00@gmail.com>
2021-08-28 00:48:53 +0900

    Update compression.py

--- Programmers/Level2/Level2_13/Compression/compression.py
+++ Programmers/Level2/Level2_13/Compression/compression.py
@@ -1,10 +1,14 @@
 def nextStr(msg,i,result):
-    result += msg[i]
-    if result in dic:
-        return i+nextStr(msg,i+1,result)
+    if i < len(msg):
+        result += msg[i]
+        if result in dic:
+            return 1 + nextStr(msg,i+1,result)
+        else:
+            answer.append(dic[result[0:-1]])
+            dic[result] = len(dic)+1
+            return 1
     else:
-        answer.append(result[0:-1])
-        dic[result] = len(dic)+1
+        answer.append(dic[result])
         return 1
 
 def solution(msg):
@@ -13,7 +17,7 @@ def solution(msg):
     dic = {chr(ord('A') + i): i + 1 for i in range(26)}
     i = 0
     while i < len(msg):
-        i = nextStr(msg,i,'')
+        i += nextStr(msg,i,'') - 1
     return answer
 
 solution('KAKAO')
