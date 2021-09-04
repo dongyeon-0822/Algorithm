3a90c751e87906c289487fca84b08d181d8184c9
DongYeon Kang <myjjue00@gmail.com>
2021-09-05 01:06:13 +0900

    Update bitsNomoreTwo.py

--- Programmers/Level2/Level2_13/BitsNoMoreTwo/bitsNomoreTwo.py
+++ Programmers/Level2/Level2_13/BitsNoMoreTwo/bitsNomoreTwo.py
@@ -2,8 +2,15 @@ def solution(numbers):
     answer = []
     for n in numbers:
         str1 = format(n,'b')
+        cnt_1 = 0
+        if n%2==0: cnt_1+=1
+        for i in reversed(range(len(str1))):
+            if str1[i] == '0':
+                break
+            else:
+                cnt_1 += 1
         while True:
-            n += 1
+            n=n+2**(cnt_1-1)
             str2 = format(n,'b')
             str1 = str1.zfill(len(str2))
             count = 0
