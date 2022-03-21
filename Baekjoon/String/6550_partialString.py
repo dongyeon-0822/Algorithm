595e27c4d7b94c8efb35cef4083d2c36530797d6
DongYeon Kang <myjjue00@gmail.com>
2022-03-22 01:55:54 +0900

    Update 6550_partialString.py

--- Baekjoon/String/6550_partialString.py
+++ Baekjoon/String/6550_partialString.py
@@ -1,18 +1,39 @@
-#반례 모르겠음
-s,t = input().split()
+import sys
+input = sys.stdin.readline
 
-result = True
-idx = 0
-if set(s)&set(t) != set(s):
-    result = False
-else:
-    for i in s:
-        if i not in t[idx:]:
-            result = False
+while True:
+    try:
+        s,t = input().rstrip().split()
+
+        # sol 1 -> 반례 못 찾음
+        # result = True
+        # idx = 0
+        # if set(s)&set(t) != set(s):
+        #     result = False
+        # else:
+        #     for i in s:
+        #         if i not in t[idx:]:
+        #             result = False
+        #         else:
+        #             idx += t[idx:].index(i)
+        # if result:
+        #     print('Yes')
+        # else:
+        #     print('No')
+
+        # sol 2
+        result = False
+        idx = 0
+        for i in range(len(t)):
+            if t[i] == s[idx]:
+                idx += 1
+            if idx == len(s):
+                result = True
+                break
+        if result:
+            print('Yes')
         else:
-            idx += t[idx:].index(i)
+            print('No')
 
-if result:
-    print('Yes')
-else:
-    print('No')
+    except:
+        break
