8b493c9a081478a04394d416b4102b913fad5dae
DongYeon Kang <myjjue00@gmail.com>
2022-04-27 23:33:06 +0900

    Modify example_1.py

--- ThisIsCodingTest/Realization/example_1.py
+++ ThisIsCodingTest/Realization/example_1.py
@@ -2,17 +2,24 @@ N = int(input())
 x, y = 1, 1
 plans = input().split()
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-move_type = ['L', 'R', 'U', 'D']
-
 for plan in plans:
-    for i in range(len(move_type)):
-        if plan == move_type[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-    if nx <1 or ny < 1 or nx > N or ny > N:
-        continue
-    x, y = nx, ny
+    if plan == 'L' and y > 1:
+        y -= 1
+    elif plan == 'R' and y < N:
+        y += 1
+    elif plan == 'U' and x > 1:
+        x -= 1
+    elif plan == 'D' and x < N:
+        x += 1
 
 print(x,y)
+
+##### answer #####
+# for plan in plans:
+#     for i in range(len(move_type)):
+#         if plan == move_type[i]:
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#     if nx <1 or ny < 1 or nx > N or ny > N:
+#         continue
+#     x, y = nx, ny
