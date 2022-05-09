3015244f04290990d11e0e831c3ae140e8695287
DongYeon Kang <myjjue00@gmail.com>
2022-05-09 23:41:59 +0900

    Update stringCompress.py

--- ThisIsCodingTest/Implementation/stringCompress.py
+++ ThisIsCodingTest/Implementation/stringCompress.py
@@ -1,24 +1,35 @@
+import math
+
+
 def solution(s):
     answers = []
-    length = range(1,len(s)//2)
+    length = range(1,math.ceil(len(s)/2) + 1)
     for l in length:
         cmp = s[:l]
         answer = l
-        count = 0
-        for i in range(0,len(s),l):
-            if len(s) < i + l: # 마지막 남았을 때
+        count = 1
+        for i in range(l,len(s),l):
+            if len(s) < i + l: # 남았을 때
+                if count != 1:
+                    answer += len(str(count))
                 answer += len(s) - i
+            elif i + l == len(s):
+                if cmp == s[i:i+l]: # 마지막이고 같은 문자열
+                    count += 1
+                    answer += len(str(count))
+                else: # 마지막이고 다른 문자열
+                    if count != 1:
+                        answer += len(str(count))
+                    answer += l
             elif cmp == s[i:i+l]: # 같은 문자열
                 count += 1
-                if i + l == len(s) and count >= 1:
-                    answer += 1
             else: # 다른 문자열
-                if count >= 1:
-                    answer += 1
+                if count != 1:
+                    answer += len(str(count))
                 answer += l
                 cmp = s[i:i+l]
-                count = 0
+                count = 1
         answers.append(answer)
     return min(answers)
 
-print(solution("ababcdcdababcdcd"))
+print(solution("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxz"))
