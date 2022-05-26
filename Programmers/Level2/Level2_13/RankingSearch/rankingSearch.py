ea100ae564f1a6a6482d7767ab47024fb8abe8e0
DongYeon Kang <myjjue00@gmail.com>
2022-05-27 00:25:39 +0900

    Update rankingSearch.py

--- Programmers/Level2/Level2_13/RankingSearch/rankingSearch.py
+++ Programmers/Level2/Level2_13/RankingSearch/rankingSearch.py
@@ -1,20 +1,33 @@
+# 다른 답을 참고하고 품
+import bisect
+
 def solution(info, query):
     answer = []
-    info_list = []
+    # 모든 경우의 수를 info_list 에 넣어준다
+    info_list = {}
+    for lang in ['cpp', 'java', 'python', "-"]:
+        for job in ['backend', 'frontend', "-"]:
+            for career in ['junior', 'senior', "-"]:
+                for food in ['chicken', 'pizza', "-"]:
+                    info_list[lang + job + career + food] = []
+    # info 를 돌면서 나올 수 있는 모든 경우를 추가해준다.
     for i in info:
-        info_list.append(i.split())
-    # info_list.sort(key=lambda x: x[4])
-    for q in query:
-        query_list = [x for x in q.split() if x != 'and']
-        arr = []
-        for i in info_list:
-            if i[0]==query_list[0] or query_list[0]=='-':
-                if i[1]==query_list[1] or query_list[1]=='-':
-                    if i[2] == query_list[2] or query_list[2] == '-':
-                        if i[3] == query_list[3] or query_list[3] == '-':
-                            if int(i[4])>= int(query_list[4]) or query_list[4] == '-':
-                                arr.append(i)
-        answer.append(len(arr))
+        tmp = i.split()
+        for lang in [tmp[0], "-"]:
+            for job in [tmp[1], "-"]:
+                for career in [tmp[2], "-"]:
+                    for food in [tmp[3], "-"]:
+                        info_list[lang + job + career + food].append(int(tmp[4]))
 
+    # 각 list 별로 점수들을 정렬해준다.
+    for key in info_list.keys():
+        info_list[key].sort()
+
+    for q in query:
+        q = q.replace(" and ", "").split() # query 와 점수로 나눈다.
+        scores = info_list[q[0]]
+        score = int(q[1])
+        # 이진 탐색
+        idx = bisect.bisect_left(scores, score)
+        answer.append(len(scores) - idx)
     return answer
-solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"],["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"])
