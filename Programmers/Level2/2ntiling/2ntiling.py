88df163aa47d8a99ffaae2afbaf438996af9c2df
DongYeon Kang <myjjue00@gmail.com>
2022-08-13 22:56:17 +0900

    Create 2ntiling.py

---
+++ Programmers/Level2/2ntiling/2ntiling.py
@@ -0,0 +1,11 @@
+def tiling(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    a, b = 1, 2
+    for i in range(3,n+1):
+        a, b = b, (a+b) % 1000000007
+    return b
+def solution(n):
+    return tiling(n)
