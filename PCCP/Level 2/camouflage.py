1b3f4f968d535cce0a57cd5fe1e154b99f2f9f22
DongYeon Kang <myjjue00@gmail.com>
2022-12-15 23:19:12 +0900

    Create camouflage.py

---
+++ PCCP/Level 2/camouflage.py
@@ -0,0 +1,11 @@
+def solution(clothes):
+    answer = 1
+    dic = {}
+    for i in clothes:
+        if i[1] in dic:
+            dic[i[1]] = dic[i[1]] + 1
+        else:
+            dic[i[1]] = 1
+    for i in dic.values():
+        answer *= (i+1)
+    return answer - 1
