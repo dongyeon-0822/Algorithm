7d8a7b70a4e88fa25a7771eb749d45d243868f01
dongyeon-0822 <myjjue00@gmail.com>
2023-03-25 00:06:21 +0900

    Update reportResult.py

--- Programmers/Level1/ReportResult/reportResult.py
+++ Programmers/Level1/ReportResult/reportResult.py
@@ -1,14 +1,27 @@
+# def solution(id_list, report, k):
+#     answer = [0] * len(id_list)
+#     result = []
+#     report = list(set(report))
+#     cnt = [0] * len(id_list)
+#
+#     for i in report:
+#         cnt[id_list.index(i.split()[1])] += 1
+#     for i, c in enumerate(cnt):
+#         if c >= k:
+#             result.append(id_list[i])
+#     for x in report:
+#         if x.split()[1] in result:
+#             answer[id_list.index(x.split()[0])] += 1
+#     return answer
+
 def solution(id_list, report, k):
     answer = [0] * len(id_list)
-    result = []
-    report = list(set(report))
-    cnt = [0] * len(id_list)
-    for i in report:
-        cnt[id_list.index(i.split()[1])] += 1
-    for i, c in enumerate(cnt):
-        if c >= k:
-            result.append(id_list[i])
-    for x in report:
-        if x.split()[1] in result:
-            answer[id_list.index(x.split()[0])] += 1
+    result = {x:0 for x in id_list}
+    report = [x.split() for x in list(set(report))]
+
+    for x,y in report:
+        result[y] += 1
+    for x,y in report:
+        if result[y] >= k:
+            answer[id_list.index(x)] += 1
     return answer
