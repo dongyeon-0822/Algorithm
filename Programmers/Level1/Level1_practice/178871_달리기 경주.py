1d8570108adcb91ee473ad1a244fdf59f4893ce8
dongyeon-0822 <myjjue00@gmail.com>
2023-06-17 23:51:51 +0900

    Update 178871_달리기 경주.py
    시간초과

--- Programmers/Level1/Level1_practice/178871_달리기 경주.py
+++ Programmers/Level1/Level1_practice/178871_달리기 경주.py
@@ -3,11 +3,14 @@ def solution(players, callings):
     dictionary = {i: players[i] for i in range(len(players))}
     for calling in callings:
         num = -1
-        for k,v in dictionary:
+        for k,v in dictionary.items():
             if v == calling:
                 num = k
                 break
         dictionary[k], dictionary[k-1] = dictionary[k-1], dictionary[k]
 
-    
+    for i in range(len(players)):
+        answer.append(dictionary[i])
     return answer
+
+print(solution(["mumu", "soe", "poe", "kai", "mine"], ["kai", "kai", "mine", "mine"]))
