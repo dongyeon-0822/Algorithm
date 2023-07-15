e57563191c6d279dc33c20d3c5f9ae4433550181
dongyeon-0822 <myjjue00@gmail.com>
2023-07-15 21:57:52 +0900

    Update maxFormula.py

--- Programmers/Level2/MaxFormula/maxFormula.py
+++ Programmers/Level2/MaxFormula/maxFormula.py
@@ -1,37 +1,34 @@
 from itertools import permutations
 
 def cal(a,b,op):
-    if op=='+':
-        return a+b
-    elif op=='-':
-        return a-b
-    elif op=='*':
-        return a*b
+    if op == '+': return a + b
+    elif op == '-': return a - b
+    elif op == '*': return a * b
 
 def solution(expression):
     answer = []
-    num = []
-    op = []
-    n = ''
+
+    nums = []
+    operators = []
+    num = ''
     for e in expression:
         if e.isdigit():
-            n+=e
+            num += e
         else:
-            num.append(int(n))
-            op.append(e)
-            n=''
-    num.append(int(n))
+            nums.append(int(num))
+            operators.append(e)
+            num = ''
+    nums.append(int(num))
 
-    for i in permutations(list(set(op)),len(set(op))):
-        _num=num.copy()
-        _op=op.copy()
-        for j in i:
-            while _op.count(j)!=0:
-                idx=_op.index(j)
-                _num[idx] = cal(_num[idx], _num[idx+1],j)
-                _num.pop(idx+1)
-                _op.pop(idx)
-        answer.append(abs(_num[0]))
+    for order_operators in permutations(list(set(operators)),len(set(operators))):
+        num = nums[:]
+        operator = operators[:]
+        for op in order_operators:
+            while (idx := ''.join(operator).find(op)) != -1:
+                num[idx] = cal(num[idx], num[idx + 1], op)
+                num.pop(idx + 1)
+                operator.pop(idx)
+        answer.append(abs(num[0]))
     return max(answer)
 
 solution("100-200*300-500+20")
