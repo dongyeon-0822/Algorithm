3d55d6d0567c1041b70d41c96f7604649e584a6c
dongyeon-0822 <myjjue00@gmail.com>
2023-07-21 04:02:18 +0900

    Update 64064_불량사용자.py

--- Programmers/Level3/64064_불량사용자.py
+++ Programmers/Level3/64064_불량사용자.py
@@ -1,7 +1,8 @@
 import re
+from itertools import product
 
 def solution(user_id, banned_id):
-    answer = 0
+    answer = set()
 
     results = [[] for _ in range(len(banned_id))]
     banned_id = [b.replace('*', '.') for b in banned_id]
@@ -9,21 +10,12 @@ def solution(user_id, banned_id):
         for j, u in enumerate(user_id):
             if re.compile(b).fullmatch(u):
                 results[i].append(j)
-
-    tmp = []
-    for i, result in enumerate(results):
-        for j, x in enumerate(result):
-            if x in tmp: continue
-            else:
-                tmp.append(x)
-                break
-        else:
-            if len(tmp) != i+1:
-                break
+    
+    for result in list(product(*results)):
+        tmp = tuple(set(result))
         if len(tmp) == len(results):
-            answer += 1
-            tmp = []
+            answer.add(tmp)
 
-    return answer
+    return len(answer)
 
 print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"],["fr*d*", "*rodo", "******", "******"]))
