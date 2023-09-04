5a203f9609073cb5a226a9c8e3602db442ba4b1b
dongyeon-0822 <myjjue00@gmail.com>
2023-09-04 23:47:27 +0900

    Update jump&teleport.py

--- Programmers/Level2/Jump&Teleport/jump&teleport.py
+++ Programmers/Level2/Jump&Teleport/jump&teleport.py
@@ -1,12 +1,9 @@
 def solution(n):
     # sol1
-    return bin(n).count(1)
+    return bin(n).count('1')
     # sol2
     ans = 0
-    while n>0:
-        if n%2:
-            n-=1
-            ans+=1
-        n//=2
+    while n:
+        ans += n % 2
+        n //= 2
     return ans
-
