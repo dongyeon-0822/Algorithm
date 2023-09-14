61414eac5ddb58b878006051c08d039268651df2
dongyeon-0822 <myjjue00@gmail.com>
2023-09-14 22:43:07 +0900

    Create 15652_N&M(4).py

---
+++ Baekjoon/BackTracking/15652_N&M(4).py
@@ -0,0 +1,19 @@
+# 중복 조합
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+
+# sol 1
+from itertools import combinations_with_replacement
+for arr in list(combinations_with_replacement(list(range(1,N+1)), M)):
+    print(*arr, sep=" ")
+
+# sol 2
+def backtracking(array, n):
+    if len(array) == M:
+        print(*array, sep=" ")
+        return
+    for i in range(n, N+1):
+        backtracking(array + [i], i)
+
+backtracking([], 1)
