6b498934248eac72f4f3e6a1e16fb16bca6cb2a5
dongyeon-0822 <myjjue00@gmail.com>
2024-06-14 23:47:52 +0900

    Update 삼각김밥.py

--- SWEA/D3/삼각김밥.py
+++ SWEA/D3/삼각김밥.py
@@ -16,7 +16,7 @@ while n <= 10000:
         col += 1
     row += 1
     arr.append(line)
-print(dic)
+# print(arr)
 
 T = int(input())
 for t in range(1, T+1):
@@ -24,19 +24,19 @@ for t in range(1, T+1):
     s,e = map(int, input().split())
     start, end = min(s,e), max(s,e)
 
-    visited = [False] * 100001
+    visited = [False] * 10001
     q = deque()
-    q.append((dic[start], 0))
+    q.append((dic[start][0], dic[start][1], 0))
     visited[start] = True
     while q:
-        (x, y), d = q.popleft()
+        x, y, d = q.popleft()
         if arr[x][y] == end:
             answer = d
             break
         for dx, dy in [[0, -1], [0, 1], [1, 0], [1, 1]]:
             nx, ny = x + dx, y + dy
-            if 0 <= ny <= nx < 140 and not visited[arr[nx][ny]]:
-                q.append(((nx, ny), d+1))
+            if 0 <= ny <= nx <= dic[end][0] and not visited[arr[nx][ny]]:
+                q.append((nx, ny, d+1))
                 visited[arr[nx][ny]] = True
 
     print('#' + str(t), answer)
