961401059c82468a74dce98a348ed03e2c6e1213
DongYeon Kang <myjjue00@gmail.com>
2024-09-23 23:15:12 +0900

    Create 25602_캔주기.py

---
+++ Baekjoon/BackTracking/25602_캔 주기.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class 25602_캔주기 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        String s = br.readline();
+
+        System.out.println(N, K, s);
+    }
